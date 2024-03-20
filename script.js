26e8a53771713bfec033f9324dd25d96996d43ea
Mathews Baio <baiomathews@gmail.com>
2024-03-20 12:34:51 -0300

    controle de tamanho das tasks, botões de editar e excluir

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let addTask = document.getElementById("plus");
 let taskList = document.querySelector(".content > ul");
+let x 
+let edit;
+
+let limit = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
 //Evento para criar input que recebera nova task
 addTask.addEventListener("click", event =>{
     let inp = document.createElement("input");
     inp.setAttribute("type","text");
+    inp.setAttribute("maxlength", limit.length - 2)
+    inp.setAttribute("size", limit.length - 2)
 
     let newTask = document.createElement("li");
     document.querySelector(".content > ul").appendChild(newTask);
@@ -15,6 +21,17 @@ addTask.addEventListener("click", event =>{
         if(event.key == 'Enter') {
             newTask.innerHTML += `<input type="checkbox"> `
             newTask.innerHTML += event.target.value
+            
+            x = document.createElement("p");
+            x.textContent = `-`
+            x.classList.add("x-class")
+            newTask.appendChild(x)
+
+            edit = document.createElement("p");
+            edit.innerHTML = `<strong class="edit-icon">/</strong> Edit task`
+            edit.classList.add("edit")
+            newTask.appendChild(edit);
+
             inp.remove();
         }
     })
